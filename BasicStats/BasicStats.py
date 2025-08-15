10b1c928252e8e67c05c73f073065f998621b834
Gerald D Silva <155676552+geraldOnGit@users.noreply.github.com>
2025-08-15 16:32:35 +0530

    Delete BasicStats directory

--- BasicStats/BasicStats.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('BasicStats/Housing.csv')
-
-print(f"this is mean of prices in housing data: {df['price'].mean()}")
-print(f"this is mode of prices in housing data: {df['price'].mode()}")
-print(f"this is median of prices in housing data: {df['price'].median()}")
-print(f"this is the minimum price in housing data: {df['price'].min()}")
-print(f"this is the maximum price in housing data: {df['price'].max()}")
